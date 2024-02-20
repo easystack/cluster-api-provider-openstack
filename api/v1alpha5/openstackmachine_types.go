cddd7f600f5ba1ddc068687bc2724688a35d9e89
junsheng.wu <junsheng.wu@easystack.cn>
2024-02-20 15:20:32 +0800

    Add DisableFloatingIP to machine spec
    * Add DisableFloatingIP to machine spec

--- api/v1alpha5/openstackmachine_types.go
+++ api/v1alpha5/openstackmachine_types.go
@@ -66,6 +66,8 @@ type OpenStackMachineSpec struct {
 	// UUID, IP address of a port from this subnet will be marked as AccessIPv4 on the created compute instance
 	Subnet string `json:"subnet,omitempty"`
 
+	DisableFloatingIP bool `json:"disableAPIServerFloatingIP"`
+
 	// The floatingIP which will be associated to the machine, only used for master.
 	// The floatingIP should have been created and haven't been associated.
 	FloatingIP string `json:"floatingIP,omitempty"`
